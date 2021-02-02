fa49e295a06d2311099c47814db35f211356f8fd
Juan Pablo Zapata <juanp.zapataa@autonoma.edu.co>
2021-02-02 22:14:59 +0000

    Modulo de autenticacion

---
+++ database/migrations/2021_02_02_215606_create_links_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('label',128);
+            $table->string('url',256);
+            $table->foreignId('user_id',128);
+
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
